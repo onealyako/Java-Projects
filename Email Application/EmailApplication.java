48e51a82c7a000abc94a70b900dcbf9de6dcf9d2
onealyako <43280185+onealyako@users.noreply.github.com>
2020-08-27 15:17:39 -0400

    Add files via upload

---
+++ Email Application/EmailApplication.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package emailapplication;
+import java.util.*;
+
+/**
+ *
+ * @author onealyako
+ */
+public class EmailApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Email em1 = new Email("John","Smithers");
+        System.out.println(em1.showInfo());
+       
+        
+         
+        
+        
+    }
+    
+}
